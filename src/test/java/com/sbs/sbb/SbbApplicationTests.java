a1bf4ee9ffd541442b8f17ac5196707a6190ef68
hyunji1205 <sguswl1205@gmail.com>
2024-05-07 15:27:59 +0900

    findBySubject

--- src/test/java/com/sbs/sbb/SbbApplicationTests.java
+++ src/test/java/com/sbs/sbb/SbbApplicationTests.java
@@ -34,11 +34,13 @@ class SbbApplicationTests {
 //		Question q = all.get(0);
 //		assertEquals("sbb가 무엇인가요?", q.getSubject());
 
-		Optional<Question> oq = this.questionRepository.findById(1);
-
-		if(oq.isPresent()) {
-			Question q = oq.get();
-			assertEquals("sbb가 무엇인가요?", q.getSubject());
-		}
+//		Optional<Question> oq = this.questionRepository.findById(1);
+//
+//		if(oq.isPresent()) {
+//			Question q = oq.get();
+//			assertEquals("sbb가 무엇인가요?", q.getSubject());
+//		}
+		Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+		assertEquals(1, q.getId());
 	}
 }
