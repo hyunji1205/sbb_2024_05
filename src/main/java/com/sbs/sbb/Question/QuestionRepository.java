b4db5146e22bd93ef8cfaeba46136af2b9fc543f
hyunji1205 <sguswl1205@gmail.com>
2024-05-08 17:07:24 +0900

    질문 상세 페이지 추가

--- src/main/java/com/sbs/sbb/Question/QuestionRepository.java
+++ src/main/java/com/sbs/sbb/Question/QuestionRepository.java
@@ -3,7 +3,6 @@ package com.sbs.sbb.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
-
     Question findBySubject(String subject);
 
     Question findBySubjectAndContent(String subject, String content);
