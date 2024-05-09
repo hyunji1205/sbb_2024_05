3c842230e11ede0a6aedb313cd3a5e7976ed5860
hyunji1205 <sguswl1205@gmail.com>
2024-05-09 14:32:05 +0900

    답변 저장 기능

--- src/test/java/com/sbs/sbb/SbbApplicationTests.java
+++ src/test/java/com/sbs/sbb/SbbApplicationTests.java
@@ -16,12 +16,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
-
+//@Rollback(true)
 class SbbApplicationTests {
 
 	@Autowired
 	private QuestionRepository questionRepository;
-
 	@Autowired
 	private AnswerRepository answerRepository;
 
@@ -40,52 +39,52 @@ class SbbApplicationTests {
 //		q2.setCreateDate(LocalDateTime.now());
 //		this.questionRepository.save(q2);  // 두번째 질문 저장
 
-//		List<Question> all = this.questionRepository.findAll();
-//		assertEquals(2, all.size());
+//        List<Question> all = this.questionRepository.findAll();
+//        assertEquals(2, all.size());
 //
-//		Question q = all.get(0);
-//		assertEquals("sbb가 무엇인가요?", q.getSubject());
+//        Question q = all.get(0);
+//        assertEquals("sbb가 무엇인가요?", q.getSubject());
 
-//		Optional<Question> oq = this.questionRepository.findById(1);
+//        Optional<Question> oq = this.questionRepository.findById(1);
+//        if(oq.isPresent()) {
+//            Question q = oq.get();
+//            assertEquals("sbb가 무엇인가요?", q.getSubject());
+//        }
+
+//        Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+//        assertEquals(1, q.getId());
+
+//        Question q = this.questionRepository.findBySubjectAndContent(
+//                "sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
+//        assertEquals(1, q.getId());
+
+//        Optional<Question> oq = this.questionRepository.findById(1);
+//        assertTrue(oq.isPresent());
+//        Question q = oq.get();
+//        q.setSubject("수정된 제목");
+//        this.questionRepository.save(q);
+
+//        assertEquals(2, this.questionRepository.count());
+//        Optional<Question> oq = this.questionRepository.findById(1);
+//        assertTrue(oq.isPresent());
+//        Question q = oq.get();
+//        this.questionRepository.delete(q);
+//        assertEquals(1, this.questionRepository.count());
+
+//        Optional<Question> oq = this.questionRepository.findById(2);
+//        assertTrue(oq.isPresent());
+//        Question q = oq.get();
 //
-//		if(oq.isPresent()) {
-//			Question q = oq.get();
-//			assertEquals("sbb가 무엇인가요?", q.getSubject());
-//		}
-//		Question q = this.questionRepository.findBySubject("sbb가 무엇인가요?");
-//		assertEquals(1, q.getId());
+//        Answer a = new Answer();
+//        a.setContent("네 자동으로 생성됩니다.");
+//        a.setQuestion(q);  // 어떤 질문의 답변인지 알기위해서 Question 객체가 필요하다.
+//        a.setCreateDate(LocalDateTime.now());
+//        this.answerRepository.save(a);
 
-//		Question q = this.questionRepository.findBySubjectAndContent(
-//				"sbb가 무엇인가요?", "sbb에 대해서 알고 싶습니다.");
-//		assertEquals(1, q.getId());
-
-//		Optional<Question> oq = this.questionRepository.findById(1);
-//		assertTrue(oq.isPresent());
-//		Question q = oq.get();
-//		q.setSubject("수정된 제목");
-//		this.questionRepository.save(q);
-
-//		assertEquals(2, this.questionRepository.count());
-//		Optional<Question> oq = this.questionRepository.findById(1);
-//		assertTrue(oq.isPresent());
-//		Question q = oq.get();
-//		this.questionRepository.delete(q);
-//		assertEquals(1, this.questionRepository.count());
-
-//		Optional<Question> oq = this.questionRepository.findById(2);
-//		assertTrue(oq.isPresent());
-//		Question q = oq.get();
-//
-//		Answer a = new Answer();
-//		a.setContent("네 자동으로 생성됩니다.");
-//		a.setQuestion(q);  // 어떤 질문의 답변인지 알기위해서 Question 객체가 필요하다.
-//		a.setCreateDate(LocalDateTime.now());
-//		this.answerRepository.save(a);
-
-//		Optional<Answer> oa = this.answerRepository.findById(1);
-//		assertTrue(oa.isPresent());
-//		Answer a = oa.get();
-//		assertEquals(2, a.getQuestion().getId());
+//        Optional<Answer> oa = this.answerRepository.findById(1);
+//        assertTrue(oa.isPresent());
+//        Answer a = oa.get();
+//        assertEquals(2, a.getQuestion().getId());
 
 		Optional<Question> oq = this.questionRepository.findById(2);
 		assertTrue(oq.isPresent());
